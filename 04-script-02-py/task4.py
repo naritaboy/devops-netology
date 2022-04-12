231d2dd7950545aaa4291f579e4267dcf525721e
Rustam Shuimbaev <say_boo@icloud.com>
2022-04-12 19:43:20 +0300

    Изменил функции task4.py

--- 04-script-02-py/task4.py
+++ 04-script-02-py/task4.py
@@ -4,36 +4,32 @@ import socket
 
 
 def f_last_check_data(file):
-    url_list = []
-    ip_list = []
+    url_ip_arr = []
     try:
         file_import = open(f'{file}')
     except FileNotFoundError:
-        return url_list, ip_list
-    last_check = file_import.read().splitlines()
+        return url_ip_arr
+    url_ip_list = file_import.read().splitlines()
     file_import.close()
-    for host in last_check:
-        url_list.append(host.split(' - ')[0])
-        ip_list.append(host.split(' - ')[1])
-    return url_list, ip_list
-
-
-def f_check_main(file, url_serv):
-    url_last_check, ip_last_check = f_last_check_data(file_last_check)
-    file_check = open(f'{file}', 'w')
-    for url_check in url_serv:
-        ip_check = socket.gethostbyname(url_check)
-        result_check = f'{url_check} - {ip_check}'
-        print(result_check)
-        file_check.write(f'{result_check}\n')
-        for i in range(len(url_last_check)):
-            if url_last_check[i] == url_check and ip_last_check[i] != ip_check:
-                print(f'[ERROR] {url_check} IP mismatch: {ip_last_check[i]} {ip_check}')
-    file_check.close()
-    return
+    for i, host in enumerate(url_ip_list):
+        url_ip_arr.append(host.split(' - '))
+    return url_ip_arr
 
 
 url_services = ['drive.google.com', 'mail.google.com', 'google.com']
 file_last_check = 'IP.txt'
 
-f_check_main(file_last_check, url_services)
+url_last_arr = f_last_check_data(file_last_check)
+file_check = open(f'{file_last_check}', 'w')
+
+for url_check in url_services:
+    ip_check = socket.gethostbyname(url_check)
+    result_check = f'{url_check} - {ip_check}'
+    print(result_check)
+    for host in url_last_arr:
+        if host[0] == url_check and host[1] != ip_check:
+            print(f'[ERROR] {url_check} IP mismatch: {host[1]} {ip_check}')
+
+    file_check.write(f'{result_check}\n')
+file_check.close()
+
