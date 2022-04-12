26896395df4d36d19430bb1f5e010d01f38abffc
Rustam Shuimbaev <say_boo@icloud.com>
2022-04-12 21:57:31 +0300

    Готовый task4.py

--- 04-script-02-py/task4.py
+++ 04-script-02-py/task4.py
@@ -1,35 +1,44 @@
 #!/usr/bin/env python3
-
+import sys
 import socket
 
 
-def f_last_check_data(file):
-    url_ip_arr = []
+def f_prev_values(file):
+    values_arr = []
     try:
         file_import = open(f'{file}')
     except FileNotFoundError:
-        return url_ip_arr
+        return values_arr
     url_ip_list = file_import.read().splitlines()
     file_import.close()
-    for i, host in enumerate(url_ip_list):
-        url_ip_arr.append(host.split(' - '))
-    return url_ip_arr
+    for host in url_ip_list:
+        values_arr.append(host.split(' - '))
+    return values_arr
 
 
+def f_match_ip(result_new, result_old):
+    for host_new, host_old in zip(result_new, result_old):
+        if host_new[0] == host_old[0] and host_new[1] != host_old[1]:
+            print(f'[ERROR] {host_new[0]} IP mismatch: {host_old[1]} {host_new[1]}')
+    return
+
+
+cur_values_arr = []
+cur_values_list = []
+param = sys.argv
+prev_values_file = 'service_check_result.txt'
 url_services = ['drive.google.com', 'mail.google.com', 'google.com']
-file_last_check = 'IP.txt'
 
-url_last_arr = f_last_check_data(file_last_check)
-file_check = open(f'{file_last_check}', 'w')
+prev_values_arr = f_prev_values(prev_values_file)
 
-for url_check in url_services:
-    ip_check = socket.gethostbyname(url_check)
-    result_check = f'{url_check} - {ip_check}'
-    print(result_check)
-    for host in url_last_arr:
-        if host[0] == url_check and host[1] != ip_check:
-            print(f'[ERROR] {url_check} IP mismatch: {host[1]} {ip_check}')
+check_result_file = open(f'{prev_values_file}', 'w')
+for i, url in enumerate(url_services):
+    ip_addr = socket.gethostbyname(url)
+    cur_values_list.insert(i, f'{url} - {ip_addr}')
+    cur_values_arr.append(cur_values_list[i].split(' - '))
+    print(cur_values_list[i])
+    check_result_file.write(f'{cur_values_list[i]}\n')
+check_result_file.close()
 
-    file_check.write(f'{result_check}\n')
-file_check.close()
-
+if len(param) > 1 and param[1] == '-m':
+    f_match_ip(cur_values_arr, prev_values_arr)
