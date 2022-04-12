4b29e1c5d21cb4c38bb68ed484ce7620f16f6a3f
Rustam Shuimbaev <say_boo@icloud.com>
2022-04-12 16:39:46 +0300

    Дорабоал task4.py

--- 04-script-02-py/task4.py
+++ 04-script-02-py/task4.py
@@ -2,32 +2,32 @@
 import socket
 
 url_services = ['drive.google.com', 'mail.google.com', 'google.com']
-url_last_check = list(range(len(url_services)))
-ip_last_check = list(range(len(url_services)))
+url_last_check = []
+ip_last_check = []
 
 try:
     file_import = open('IP.txt')
     last_check = file_import.read().splitlines()
     file_import.close()
 
-    for i in range(len(last_check)):
-        host = last_check[i].split(' - ')
-        url_last_check[i] = host[0]
-        ip_last_check[i] = host[1]
+    for host in last_check:
+        url_last_check.append(host.split(' - ')[0])
+        ip_last_check.append(host.split(' - ')[1])
 
 except FileNotFoundError:
     print('First start script. An IP.txt file has been created in the current directory.')
 
 file_check = open('IP.txt', 'w')
 
-for service in url_services:
-    ip_check = socket.gethostbyname(service)
-    result_check = f'{service} - {ip_check}'
+for url_check in url_services:
+    ip_check = socket.gethostbyname(url_check)
+    result_check = f'{url_check} - {ip_check}'
     print(result_check)
     file_check.write(f'{result_check}\n')
 
-    for j in range(len(url_last_check)):
-        if url_last_check[j] == service and ip_last_check[j] != ip_check:
-            print(f'[ERROR] {service} IP mismatch: {ip_last_check[j]} {ip_check}')
+    for i in range(len(url_last_check)):
+        if url_last_check[i] == url_check and ip_last_check[i] != ip_check:
+            print(f'[ERROR] {url_check} IP mismatch: {ip_last_check[i]} {ip_check}')
 
 file_check.close()
+
